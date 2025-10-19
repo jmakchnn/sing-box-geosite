7fcc7c5b4f9f48202b1f423390b6b756ac87b5a0
jmakchnn <73983690+jmakchnn@users.noreply.github.com>
2025-10-19 11:19:29 +0800

    Add files via upload

---
+++ release-rule-set.sh
@@ -0,0 +1,21 @@
+# https://github.com/SagerNet/sing-geosite/blob/main/.github/release-rule-set.sh
+
+#!/bin/bash
+
+set -e -o pipefail
+
+
+for file in output/*.json; do
+    ./sing-box rule-set compile "$file"
+done
+
+
+cd output
+git init
+git config --local user.email "github-action@users.noreply.github.com"
+git config --local user.name "GitHub Action"
+git remote add origin https://github-action:$GITHUB_TOKEN@github.com/xmdhs/sing-box-ruleset.git
+git branch -M rule-set
+git add .
+git commit -m "Update rule-set"
+git push -f origin rule-set
